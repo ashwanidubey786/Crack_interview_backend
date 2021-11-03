37293a17e30f88c559671d3d81198e75b732a90d
ashwanidubey <ashwani786dubey@gmail.com>
2021-11-03 10:04:29 +0530

    done

--- config/databaseConnection.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config()
-const {MongoClient}=require('mongodb')
-
-const databaseName=process.env.DATABASENAME;
-const host=process.env.HOST;
-console.log(databaseName);
-console.log(host);
